c07dec68554e93cc1c55429b237767ec07be14f0
shaxrizodaisoyeva <shaxrizodaisoyeva24@gmail.com>
2023-01-04 13:39:46 +0500

    8

--- Index08.py
+++ Index08.py
@@ -6,5 +6,17 @@ def main(s):
     Returns:
         int: answer
     """
-    return
+    x = False
+    if s[0]=="*":
+        x = 0
+    if s[1]=="*":
+        x = 1
+    if s[2]=="*":
+        x = 2
+    if s[3]=="*":
+        x = 3
+    if s[4]=="*":
+        x = 4
+    return x
+
         
