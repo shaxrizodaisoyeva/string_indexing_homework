23907dab1e190ee6950ea840a5011f6f6904088b
shaxrizodaisoyeva <shaxrizodaisoyeva24@gmail.com>
2023-01-04 13:44:19 +0500

    9

--- Index09.py
+++ Index09.py
@@ -6,4 +6,8 @@ def main(s):
     Returns:
         int: answer
     """
-    return
+    if s == "0" or s == "1" or s == "2" or s == "3" or s == "4" or s == "5" or s == "6" or s == "7" or s == "8" or s == "9":
+        x = int(s)
+    else:
+        x = -1
+    return x
